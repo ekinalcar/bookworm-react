94edf3b83330a12d05205c3d928e35f23c3e7a27
ekinalcar <ekinalcar@firmasoft.com>
2019-06-14 14:40:57 +0300

    semantic ui

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import HomePage from "./components/pages/HomePage";
 import LoginPage from "./components/pages/LoginPage";
 
 const App = () => <div>
-  <Route path="/" exact component={HomePage} />
-  <Route path="/login" exact component={LoginPage} />
+  <div className="ui container">
+    <Route path="/" exact component={HomePage} />
+    <Route path="/login" exact component={LoginPage} />
+  </div>
 </div>;
 
 export default App;
